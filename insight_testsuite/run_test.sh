edf9c9a8d44830b80f10ae9bf04f1741bc082c7c
Ji Li <jili@uark.edu>
2016-11-10 22:45:48 -0600

    version 1

---
+++ insight_testsuite/run_test.sh
@@ -0,0 +1,10 @@
+python ../src/FraudulentDetector1.py ./tests/test-1-paymo-trans/paymo_input/stream_payment.csv ./tests/test-1-paymo-trans/paymo_input/batch_payment.csv ./tests/test-1-paymo-trans/paymo_output/output1.csv
+python ../src/FraudulentDetector2.py ./tests/test-1-paymo-trans/paymo_input/stream_payment.csv ./tests/test-1-paymo-trans/paymo_input/batch_payment.csv ./tests/test-1-paymo-trans/paymo_output/output2.csv
+python ../src/FraudulentDetector3.py ./tests/test-1-paymo-trans/paymo_input/stream_payment.csv ./tests/test-1-paymo-trans/paymo_input/batch_payment.csv ./tests/test-1-paymo-trans/paymo_output/output3.csv
+
+python ../src/FraudulentDetector1.py ./tests/Ji Li-test/paymo_input/stream_payment.csv ./tests/Ji Li-test/paymo_input/batch_payment.csv ./tests/Ji Li-test/paymo_output/output1.csv
+python ../src/FraudulentDetector2.py ./tests/Ji Li-test/paymo_input/stream_payment.csv ./tests/Ji Li-test/paymo_input/batch_payment.csv ./tests/Ji Li-test/paymo_output/output2.csv
+python ../src/FraudulentDetector3.py ./tests/Ji Li-test/paymo_input/stream_payment.csv ./tests/Ji Li-test/paymo_input/batch_payment.csv ./tests/Ji Li-test/paymo_output/output3.csv
+
+
+
---
+++ run.sh
@@ -0,0 +1,5 @@
+python ./src/FraudulentDetector1.py ./paymo_input/stream_payment.csv ./paymo_input/batch_payment.csv ./paymo_output/output1.csv
+python ./src/FraudulentDetector2.py ./paymo_input/stream_payment.csv ./paymo_input/batch_payment.csv ./paymo_output/output2.csv
+python ./src/FraudulentDetector3.py ./paymo_input/stream_payment.csv ./paymo_input/batch_payment.csv ./paymo_output/output3.csv
+
+
